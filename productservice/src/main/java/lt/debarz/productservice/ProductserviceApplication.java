c8654e0e046d1d1f8be271c685fc0c5f2d44cc11
deividasbardzenis <deividasbarzdenis@yahoo.com>
2022-02-04 19:17:35 +0200

    first commit

---
+++ productservice/src/main/java/lt/debarz/productservice/ProductserviceApplication.java
@@ -0,0 +1,13 @@
+package lt.debarz.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductserviceApplication.class, args);
+    }
+
+}
