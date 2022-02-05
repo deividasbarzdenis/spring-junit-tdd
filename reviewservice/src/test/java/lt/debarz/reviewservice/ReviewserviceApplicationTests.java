a5012ed7dca95e60ce2cc598f773bd3b9546a7f2
deividasbardzenis <deividasbarzdenis@yahoo.com>
2022-02-05 22:33:21 +0200

    Added review service module

---
+++ reviewservice/src/test/java/lt/debarz/reviewservice/ReviewserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.debarz.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
